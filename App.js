b8cd209e834f709f4963700391e040784e8e3c6d
clemoll <60341116+clemoll@users.noreply.github.com>
2023-06-23 09:51:12 +0200

    feat: ajout de boutton rapide

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React from 'react';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>E-Prescrib</Text>
       <StatusBar style="auto" />
+      <Button
+        title="Voie Rapide"
+        color="red"
+        tintColor="lightblue"
+      />
     </View>
   );
 }
@@ -14,7 +20,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems:'center',
+    marginTop: '10%',
   },
 });
